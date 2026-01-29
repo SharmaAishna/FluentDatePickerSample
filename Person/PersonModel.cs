bbbcd48eb31bcd6fe636aead9de0d379c13b5e0f
Sharma <aishna.sharma@kredit.nl>
2026-01-29 15:27:24 +0100

    Initial Commit

---
+++ Person/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class PersonModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date1  { get; set; }
+        public DateTime Date2 { get; set; }
+        public DateTime Date3 { get; set; }
+    }
+}

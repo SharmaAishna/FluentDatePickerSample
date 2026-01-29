398a44e27b233d429c8cab37479f7b85c7bbdb5d
Sharma <aishna.sharma@kredit.nl>
2026-01-29 15:04:44 +0100

    Add project files.

---
+++ ModelPerson/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelPerson
+{
+    public class Class1
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date1 { get; set; }
+        public DateTime Date2 { get; set; }
+        public DateTime Date3 { get; set; }
+        public bool radio1 { get; set; }
+    }
+}
